a00ad70991f2da8c81db3af24dd7f0add5e11b04
Myr730 <A01735601@tec.mx>
2025-09-29 15:24:26 -0600

    Cuarto commit

--- webapi.jl
+++ webapi.jl
@@ -2,14 +2,18 @@ include("fantasma.jl")
 using Genie, Genie.Renderer.Json, Genie.Requests, HTTP
 using UUIDs
 
-route("/run") do
-    run!(model, 1)
+route("/run", method = GET) do
+    Agents.step!(model, 1)
     agents = []
     for ghost in allagents(model)
-        push!(agents, ghost)
+        push!(agents, Dict(
+            :id => ghost.id,
+            :pos => ghost.posicion
+            :type => ghost.type
+        ))
     end
 
-    json(Dict(:msg => "Adios", "agents" => agents))
+    json(Dict(:agents => agents))
 end
 
 Genie.config.run_as_server = true
@@ -18,4 +22,4 @@ Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
 Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
 Genie.config.cors_allowed_origins = ["*"]
 
-up()
+Genie.startup(8000, "0.0.0.0")
